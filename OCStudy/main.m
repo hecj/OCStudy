88aacbccc1cbd3e52798afd065ac05a09f69a4f4
hechaojie <275070023@qq.com>
2016-08-23 21:24:24 +0800

    Initial Commit

---
+++ OCStudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCStudy
+//
+//  Created by hechaojie on 16/8/23.
+//  Copyright © 2016年 hechaojie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
